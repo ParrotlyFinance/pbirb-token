f7caa9ac7ca0f6f9fdef56e83c914b68aa92d769
greatfox99 <matt.r.merrill@gmail.com>
2022-02-17 23:13:59 -0600

    update solidity compiler. rename contract file

--- contracts/Parrotly.sol
+++ contracts/Parrotly.sol
@@ -70,7 +70,7 @@
  *  Twitter -------- https://twitter.com/ParrotlyFinance
 **/
 
-pragma solidity ^0.8.11;
+pragma solidity 0.8.12;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
--- truffle-config.js
+++ truffle-config.js
@@ -85,7 +85,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: '^0.8.2',
+      version: '0.8.12',
       settings: {
         evmVersion: 'byzantium' // Default: "petersburg"
       },
